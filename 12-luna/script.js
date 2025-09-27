fb9a22ff21f99a5b4d197945f61abd89c701000a
alirenIrina <aliren.irina@yandex.ru>
2025-09-27 12:06:17 +0700

    Homework №12 '12-luna'

---
+++ 12-luna/script.js
@@ -0,0 +1,29 @@
+const card = '4561-2612-1234-5464';
+
+let sumNum = 0;
+
+function checkCard(card) {
+    card = card.replaceAll('-', '');
+
+    for (let i = 0; i < card.length; i += 1) {
+        if (((i + 1) % 2) !== 0) {
+            let oddNum = Number(card[i]) * 2;
+                if (oddNum > 9) {
+                oddNum -= 9;
+                }
+            sumNum += oddNum;
+        } else {
+            let evenNum = Number(card[i]);
+            sumNum += evenNum;
+        }
+    }
+
+    if ((sumNum % 10) == 0) {
+        return true;
+    }
+
+    return false;
+}
+
+
+console.log(checkCard(card));
